d39dde6709ad8c2600d5a41a3c6c2a261dd76d79
VladBrok <vlad.brok99@gmail.com>
2023-08-20 13:37:54 +0300

    add site title, description and favicons

--- app.vue
+++ app.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <NuxtWelcome />
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: "Medical history",
+  link: [
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/favicons/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicons/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicons/favicon-16x16.png",
+    },
+    { rel: "manifest", href: "/site.webmanifest" },
+  ],
+  meta: [
+    { name: "msapplication-TileColor", content: "#da532c" },
+    { name: "theme-color", content: "#ffffff" },
+    { name: "description", content: "A medical history website." },
+  ],
+});
+</script>
