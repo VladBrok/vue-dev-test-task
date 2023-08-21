0e733d7a3d4032f2022273c620b3521ff6f46829
VladBrok <vlad.brok99@gmail.com>
2023-08-21 15:15:26 +0300

    seed database

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
+
+const prisma = new PrismaClient();
+
+const PATIENTS_COUNT = 10;
+
+async function main() {
+  await prisma.doctorPatient.deleteMany();
+  await prisma.doctor.deleteMany();
+  await prisma.patient.deleteMany();
+
+  await Promise.all(
+    Array(PATIENTS_COUNT)
+      .fill(null)
+      .map(() =>
+        prisma.patient.create({
+          data: {
+            name: faker.person.fullName(),
+            email: faker.internet.email(),
+            image: faker.image.avatar(),
+          },
+        }),
+      ),
+  );
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
