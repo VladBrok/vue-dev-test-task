2ec7c592058c55f70c9b648444a9e789736082ba
VladBrok <vlad.brok99@gmail.com>
2023-08-21 10:53:01 +0300

    setup auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,26 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+  modules: ["@nuxt-alt/auth", "@pinia/nuxt"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  auth: {
+    redirect: {
+      login: "/auth/login",
+      logout: "/auth/logout",
+      home: "/",
+      callback: "/auth/callback",
+    },
+    strategies: {
+      google: {
+        clientId: process.env.CLIENT_ID,
+        codeChallengeMethod: "",
+        responseType: "token id_token",
+      },
     },
   },
 });
